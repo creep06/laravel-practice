c790548e7268b57508a7adcfd281082351284406
Kuwata <creep040@gmail.com>
2019-05-12 17:41:04 +0900

    heroku向け調整

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,29 +6,34 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-	/**
-	 * Bootstrap any application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		// sqliteで外部キー制約を有効にする
-		if (\DB::getDriverName() == 'sqlite') {
-			\DB::statement(\DB::raw('PRAGMA foreign_keys=1'));
-		}
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // sqliteで外部キー制約を有効にする
+        if (\DB::getDriverName() == 'sqlite') {
+            \DB::statement(\DB::raw('PRAGMA foreign_keys=1'));
+        }
 
-		// グローバル変数 管理者のidを1とする
-		config(['admin_id' => 1]);
-	}
+        // グローバル変数 管理者のidを1とする
+        config(['admin_id' => 1]);
 
-	/**
-	 * Register any application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+        // herokuでhttps強制
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 }
