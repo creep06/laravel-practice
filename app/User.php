ef04f82edae2c238bf909936b664207d4a68d92c
Kuwata <creep040@gmail.com>
2019-06-09 16:17:34 +0900

    authバグってた

--- app/User.php
+++ app/User.php
@@ -3,8 +3,8 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Auth\MustVerifyEmail;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
